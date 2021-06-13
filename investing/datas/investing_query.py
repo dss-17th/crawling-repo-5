6e97dfc09c54ee88fb6807f397d7832a55628597
gu3735 <rluvu2@nate.com>
2021-06-13 22:11:41 +0900

    changing crawling query code

--- investing/datas/investing_query.py
+++ investing/datas/investing_query.py
@@ -2,24 +2,18 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import re
-symbols = pd.read_csv('symbol.csv')
-frames = []
-a = 1
 
-for data in symbols.values:
-    symbol = data[0]
-    name = data[1]
+
+def crawlingquery(symbol):
     url = f"https://investing.com/search/?q={symbol}"
-    headers = {"User-Agent" : ""}
+    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
     response = requests.get(url, headers=headers)
-    time.sleep(1)
-    
 
     # 맞는 데이터 찾기
     dom = BeautifulSoup(response.content, "html.parser")
     datas = dom.find_all(class_ = "js-inner-all-results-quote-item row")
     investing_query = np.nan
-    
+
     for i in range(len(datas)):
         flag = datas[i].find(class_= "flag first")
         try:
@@ -27,30 +21,15 @@ for data in symbols.values:
                 continue
         except:
             continue
-        
+
         investingsymbol = datas[i].find(class_= "second").text
         if investingsymbol.upper() != symbol.upper() :
             continue
-        
-        #investingname = datas[i].find(class_= "third").text
-        #if investingname != name :
-        #    continue
-        
+
         text = datas[i].find(class_= "fourth").text
         if text[:5] != 'Stock':
             continue
-        
-        
-        investing_query = re.findall('equities\/([\S]+)', datas[i].get("href"))[0]
-        frames.append([symbol, investing_query])
-        #break
-        
-    if a != len(frames) :
-        print([a, len(frames)])
-        break
-    print(a)
-    a += 1
-    
-dfs = pd.DataFrame(dfs)
-dfs.replace(columns = {0:'Symbol', 1:'Query'})
-dfs.to_csv('investing_query')
+
+
+        investing_query = re.findazll('equities\/([\S]+)', datas[i].get("href"))[0]
+        return investing_query
