3c74e4a6a1b0b430d58d90b8ac56b9a3f86cb44c
gu3735 <rluvu2@nate.com>
2021-06-14 12:59:11 +0900

    -

--- investing/modules/investing_query.py
+++ investing/modules/investing_query.py
@@ -6,7 +6,7 @@ import re
 
 def crawlingquery(symbol):
     url = f"https://investing.com/search/?q={symbol}"
-    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+    headers = {"User-Agent" : "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
 
     # 맞는 데이터 찾기
