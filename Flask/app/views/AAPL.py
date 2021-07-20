6aa23dcb7a01da0874c6daae9b562fd885fbbf9e
gu3735 <rluvu2@nate.com>
2021-07-20 18:36:48 +0900

    templates, views update

--- Flask/app/views/AAPL.py
+++ Flask/app/views/AAPL.py
@@ -2,8 +2,7 @@ from flask import *
 from app.modules import MysqlModule
 from app import app
 
-
-@app.route("/<symbol>") 
+@app.route("/<symbol>")
 def page(symbol):
     db_class= MysqlModule.Database()
 
@@ -11,17 +10,4 @@ def page(symbol):
 	FROM News \
 	WHERE Symbol = '{symbol}';"
     data1 = db_class.executeAll(sql)
-    
-    sql = f"SELECT * \
-	FROM daily \
-	WHERE Symbol = '{symbol}';"
-    data2 = db_class.executeAll(sql)
-
-    line_xlabels = [data['Date'] for data in data2[:100]]
-    line_data = [data['Close'] for data in data2[::-1][:100]]
-
-    return render_template('index.html',data1=data1, data2=line_data, line_xlabels=line_xlabels)
-
-
-
-
+    return render_template("index.html",data1=data1)
---
+++ Flask/app/views/apis.py
@@ -0,0 +1,50 @@
+from flask import *
+from app.modules import MysqlModule
+from app import app
+
+
+@app.route("/data/<symbol>") 
+def data(symbol):
+    result = {'code': 200}
+    term = request.values.get('term')
+    
+
+
+    db_class= MysqlModule.Database()
+
+    sql = f"SELECT * \
+	FROM News \
+	WHERE Symbol = '{symbol}';"
+    data1 = db_class.executeAll(sql)
+    
+    if term == 'week':
+        sql = f"SELECT * \
+        FROM US_Stock.daily \
+        WHERE Symbol = 'AAPL'\
+        order by DATE DESC\
+        LIMIT 6;"
+    elif term == 'month':
+        sql = f"SELECT * \
+        FROM US_Stock.daily \
+        WHERE Symbol = 'AAPL'\
+        order by DATE DESC\
+        LIMIT 22;"
+    elif term == 'month3':
+        sql = f"SELECT * \
+        FROM US_Stock.daily \
+        WHERE Symbol = 'AAPL'\
+        order by DATE DESC\
+        LIMIT 64;"
+    elif term == 'year':
+        sql = f"SELECT * \
+        FROM US_Stock.daily \
+        WHERE Symbol = 'AAPL'\
+        order by DATE DESC\
+        LIMIT 260;"
+    
+    data2 = db_class.executeAll(sql)[::-1]
+    
+    result['term'] = term
+    result['symbol'] = symbol
+    result['data2'] = data2
+    return jsonify(result)
