22b226354ead4b4181d94463f061867d656267d2
LEEJIYOUNG <ljy4292@naver.com>
2021-07-29 17:38:01 +0900

    fix date display

--- Flask/app/views/SP500.py
+++ Flask/app/views/SP500.py
@@ -7,7 +7,7 @@ def sp():
     db_class= MysqlModule.Database()
 
 ## 1. s&p500 종가 시계열 그래프
-    sql1 = "SELECT * \
+    sql1 = "SELECT date_format(daily.Date, '%%Y-%%m-%%d') as Date, Close \
     FROM daily \
     WHERE Symbol = 'US500';"
     data1 = db_class.executeAll(sql1)
